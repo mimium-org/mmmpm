256d1bac3bfea7dad4fa1901a284cc4891e88c2a
t-sin <shinichi.tanaka45@gmail.com>
2021-03-06 04:49:38 +0900

    DEVELOP: Parse Git reposiotry; it includes specification for Git repository designator

--- src/package.rs
+++ src/package.rs
@@ -1,8 +1,29 @@
+use git2::Repository;
+use log::error;
 use std::path::Path;
 
 // TODO: implement fmt::Display
 #[derive(Debug)]
 pub enum Package {
     Pkg(String),
+    Git { host: String, path: String },
     Path(Box<Path>),
+}
+
+pub fn package_from_string(pkg: String) -> Result<Package, ()> {
+    if let Some(_) = pkg.find(':') {
+        let vec: Vec<&str> = pkg.splitn(2, ":").collect();
+        if vec.len() == 2 {
+            Ok(Package::Git {
+                host: vec.get(0).unwrap().to_string(),
+                path: vec.get(1).unwrap().to_string(),
+            })
+        } else {
+            error!("mulformed package designator");
+            Err(())
+        }
+    } else {
+        error!("not supported!");
+        Err(())
+    }
 }
