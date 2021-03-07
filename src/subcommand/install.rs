7ed79c97f41279c42f333871c7560cafbf46ffe4
t-sin <shinichi.tanaka45@gmail.com>
2021-03-07 19:20:38 +0900

    DEVELOP: Remove cloned repository when it is invalid as mimium package

--- src/subcommand/install.rs
+++ src/subcommand/install.rs
@@ -1,4 +1,5 @@
-use std::path::PathBuf;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 use clap::ArgMatches;
 use git2::Repository;
@@ -18,27 +19,71 @@ fn parse_option(matches: &ArgMatches) -> Result<CmdOption, ()> {
     }
 }
 
-fn proc(mimium_dir: PathBuf, opt: CmdOption) -> Result<(), ()> {
-    match opt.package {
-        Package::Git { host, path } => {
-            info!("install {:?} from {:?} as Git repository.", path, host);
+fn clone_git_repo(mimium_dir: PathBuf, host: String, path: String) -> Result<String, ()> {
+    let repo_url = format!("https://{}/{}.git", host, path);
+    // TODO: Protection from the directory traversal attack?
+    let pkg_path = format!("{}/{}/{}", mimium_dir.to_str().unwrap(), host, path);
 
-            let repo_url = format!("https://{}/{}.git", host, path);
-            // TODO: Protection from the directory traversal attack?
-            let pkg_dir = format!("{}/{}/{}", mimium_dir.to_str().unwrap(), host, path);
+    info!("Cloning into {:?}...", pkg_path);
+    // NOTE: libgit2 cannot shallow clone...
+    match Repository::clone(&repo_url, pkg_path.clone()) {
+        Ok(_repo) => {
+            info!("Successfuly cloned package as Git repository.");
+            Ok(pkg_path)
+        }
+        Err(e) => {
+            error!("Cannot clone Git repository {:?} because {:?}", repo_url, e);
+            Err(())
+        }
+    }
+}
 
-            info!("Cloning into {:?}...", pkg_dir);
-            match Repository::clone(&repo_url, pkg_dir) {
-                Ok(repo) => {
-                    info!("Successfuly cloned package as Git repository.");
-                    Ok(())
-                }
-                Err(e) => {
-                    error!("Cannot clone Git repository {:?}", repo_url);
-                    Err(())
-                }
+fn is_mimium_package(pkg_path: &PathBuf) -> bool {
+    info!("Validating repository cloned is a mimium package.");
+    match fs::read_dir(pkg_path) {
+        Ok(mut entries) => {
+            if let Some(_) = entries.find(|e| e.as_ref().unwrap().file_name() == "mmmp.toml") {
+                info!("'mmmp.toml' is found.");
+                // TODO: is this package loadable?
+                true
+            } else {
+                info!("'mmmp.toml' is not found.");
+                false
             }
         }
+        Err(err) => {
+            error!("Read error: {:?}", err);
+            false
+        }
+    }
+}
+
+fn install_git_repo(mimium_dir: PathBuf, host: String, path: String) -> Result<(), ()> {
+    info!("install {:?} from {:?} as Git repository.", path, host);
+    match clone_git_repo(mimium_dir, host, path.clone()) {
+        Ok(pkg_path) => {
+            let pkg_path = PathBuf::from(pkg_path);
+            if is_mimium_package(&pkg_path) {
+                Ok(())
+            } else {
+                error!("The repository {} is not a mimium package.", path.clone());
+                info!("Removing the repository {}...", path.clone());
+                if let Err(err) = fs::remove_dir_all(pkg_path.clone()) {
+                    error!("Cannot remove repository because {}", err);
+                }
+                Err(())
+            }
+        }
+        err => {
+            error!("Clone error: {:?}", err);
+            Err(())
+        }
+    }
+}
+
+fn proc(mimium_dir: PathBuf, opt: CmdOption) -> Result<(), ()> {
+    match opt.package {
+        Package::Git { host, path } => install_git_repo(mimium_dir, host, path),
         Package::Pkg(_name) => Err(()),
         Package::Path(_path) => Err(()),
     }
